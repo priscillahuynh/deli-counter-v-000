ace932f2ac5d8a991331520d80118b914fb6b94b
Priscilla H <priscillamhuynh@gmail.com>
2018-06-22 13:46:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+katz_deli=["Logan","Avi","Spencer"]
+
+def line(array)
+  phrase = "The line is currently:"
+  if array.length==0
+    puts "The line is currently empty."
+  else array.length>0
+    array.each_with_index do |name, index|
+    puts phrase+="#{index+1}.#{name} "
+    end
+  end
+end
