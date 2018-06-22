3054268fab58b99495e2eb29c90699df3224a18a
Priscilla H <priscillamhuynh@gmail.com>
2018-06-22 13:35:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+katz_deli=[]
+
+def line(array)
+  phrase = "The line is currently:"
+  if array.length==0
+    puts "The line is currently empty."
+  else array.length>0
+    array.each_with_index do |name, index|
+    puts phrase+="#{index+1}. #{name}"
+    end
+  end
+end
+
+def take_a_number(array,name)
+  if array.length==0
+    array<<name
+    puts "Welcome, #{name}. You are number 1 in line."
+  elsif array.length>0
+    # array.each_with_index do |name,index|
+      array<<name
+    phrase = "Welcome, #{name}. You are number #{array.length} in line."
+    puts phrase
+  end
+end
