564100e12ab0abfead565db40d5fc12318166ace
Priscilla H <priscillamhuynh@gmail.com>
2018-06-22 13:51:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(array)
     puts "The line is currently empty."
   else array.length>0
     array.each_with_index do |name, index|
-    phrase<<"#{index+1}. #{name} "
+    phrase<<"#{index+1}. #{name}"
     end
     puts phrase
   end
