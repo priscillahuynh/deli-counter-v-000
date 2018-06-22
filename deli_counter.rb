abec920353334d64f9fe53d5e2b3adf2d3e77cb2
Priscilla H <priscillamhuynh@gmail.com>
2018-06-22 13:59:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,10 @@ def take_a_number(array,name)
   array.push(name)
   puts "Welcome, #{name}. You are number #{array.length} in line."
 end
+
+def now_serving(array)
+  if array.length==0
+    puts "There is nobody waiting to be served."
+  else 
+    puts "Currently serving " + array[0]+"."
+end
