b058ea9bbdbbcc685f46ac985648ab9a7b8112e5
Priscilla H <priscillamhuynh@gmail.com>
2018-06-22 13:57:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,4 +15,6 @@ end
 def take_a_number(array,name)
   array.push(name)
   puts "Welcome, #{name}. You are number #{array.length} in line."
+  if array.length == 0
+    puts "The line is empty"
 end
